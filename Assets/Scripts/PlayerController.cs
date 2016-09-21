d5989133f013c9588e14c88d33f19810dbf50a99
nstone007 <nstone007@yahoo.com>
2016-09-21 11:21:22 -0500

    Main Unity Files
    this is the initial upload to work on this project.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private PlayerController playerCharacter;
+    private bool isJump;
+
+    void Awake()
+    {
+        playerCharacter = GetComponent<PlayerController>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
